49eb1eae58b5053b064f330f265c79c0e52b76e5
Diegodamata <Diegodamata>
2025-01-15 17:15:57 -0300

    Projeto criado

---
+++ src/main/java/com/coderbank/portalcliente/PortalclienteApplication.java
@@ -0,0 +1,12 @@
+package com.coderbank.portalcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalclienteApplication.class, args);
+	}
+}
